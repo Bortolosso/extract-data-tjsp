0bb480c00437d843bfc769e3a415eaed48683e43
Bortolosso <bortolossojoao@gmail.com>
2019-07-22 14:14:38 -0300

    correcao de bug, file existente

--- selenium_spider.py/src/tenta.py
+++
@@ -1,9 +0,0 @@
-import os.path
-import os
-
-file_path = '/home/bortolossohurst/Documents/ambv_boot/selenium_spider.py/format_pdf/0000182-15.2001.8.26.0272/precatorio.pdf'
-
-validate = os.path.exists(file_path)
-
-print(validate)
-
